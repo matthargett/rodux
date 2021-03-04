d87082fd779ef7d1832201b5f6ee7a29cee9a3b9
Matt Hargett <plaztiksyke@gmail.com>
2021-03-03 16:23:24 -0800

    Don't analyze Packages.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,6 +4,9 @@ set -ex
 
 rojo build test-bundle.project.json --output rodux-tests.rbxmx
 
+echo "Remove .robloxrc from jest-roblox"
+rm -f Packages/_Index/roblox_testez/testez/.robloxrc
+
 roblox-cli analyze test-bundle.project.json
 selene src
 
